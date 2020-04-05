861e6afb5adcc66d2c572c629cb6ec8bd2f72029
Sobira <sobira204@gmail.com>
2020-04-05 19:06:19 +0500

    create class Employee

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Employee{
+            public string name {get; set;}
+            public string surname {get; set;}
+            public Employee (string Name, string Surname){
+                name = Name;
+                surname = Surname;
+            }
+            public Employee(){
+
+            }
+            
+
+        }
+    }
+}
