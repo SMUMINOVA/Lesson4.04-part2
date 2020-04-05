e0355279f95e0fd2402d7f658d858c4c29065420
Sobira <sobira204@gmail.com>
2020-04-05 20:16:03 +0500

    fix some bug and add new field

--- Program.cs
+++ Program.cs
@@ -9,14 +9,13 @@ namespace part2
             Employee firstEmployee = new Employee();
             System.Console.Write("Enter employee name: ");
             firstEmployee.name = Console.ReadLine();
-            System.Console.Write("\nEnter employee surname: ");
+            System.Console.Write("Enter employee surname: ");
             firstEmployee.surname = Console.ReadLine();
-            System.Console.WriteLine("\nchoose his/her position:");
-            System.Console.WriteLine("1. intern\t 2. junior\n 3. middle\t 4. senior");
+            System.Console.WriteLine("choose his/her position:");
+            System.Console.WriteLine("1. intern\t 2. junior\n3. middle\t 4. senior");
             int choicePost = int.Parse(Console.ReadLine());
-            //salary = firstEmployee.post[(choice-1)];
-            System.Console.WriteLine("\nchoose his/her experience:");
-            System.Console.WriteLine("1. 1 year\t 2. 2 year\n 3. 3 year\t 4. 4 year");
+            System.Console.WriteLine("choose his/her experience:");
+            System.Console.WriteLine("1. 1 year\t 2. 2 year\n3. 3 year\t 4. 4 year");
             int choiceExperience = int.Parse(Console.ReadLine());
             System.Console.WriteLine("Information about employee:");
             System.Console.WriteLine($"Name: {firstEmployee.name}");
@@ -35,10 +34,11 @@ namespace part2
             public Employee(){
 
             }
-            public int[] post = {1500, 2500, 3500, 5000};
+            public int[] postSalary = {1500, 2500, 3500, 5000};
+            public string[] post = {"intern", "junior", "middle", "senior"};
             public int[] service = {200, 400, 600, 800};   
             public int salary(int cP, int cE){
-                return post[cP] + service[cE];
+                return postSalary[cP-1] + service[cE-1];
             }
             public double tax(int salary){
                 return 0.14*salary;
